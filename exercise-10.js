561417977c3963be3ea513ad7532c0644a7c48b6
muhafaa <muhafa99@gmail.com>
2019-11-06 23:27:06 +0700

    update

--- exercise-10.js
+++ exercise-10.js
@@ -1,8 +1,25 @@
 function changeMe(arr) {
     // you can only write your code here!
-    var year = new Date().getFullYear();
-    var obj = {
-    };
+    if (arr.length == 0) { // jika array kosong
+      return console.log("");
+    }
+    var year = new Date().getFullYear(); // tahun sekarang
+    var obj = [{},{}];
+    for (var i = 0; i < arr.length; i++) { // masukkan data dari arr ke obj
+      obj[i].firstName = arr[i][0];
+      obj[i].lastName = arr[i][1];
+      obj[i].gender = arr[i][2];
+      if (arr[i][3] != undefined) { // jika value tahun ada
+        obj[i].age = year - arr[i][3];
+      } else {                      // jika tidak ada
+        obj[i].age = 'Invalid Birth Year';
+      }
+    }
+
+    for (i = 0; i < obj.length; i++) { // log obj
+      console.log(i+1+'. '+obj[i].firstName+' '+obj[i].lastName+':');
+      console.log(obj[i]);
+    }
 }
   
   // TEST CASES
